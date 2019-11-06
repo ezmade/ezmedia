8df7f8bc99f6a24b17533aa081b55615da638c8b
Anton Boyko <antoneasymade@gmail.com>
2019-11-06 07:52:56 +0500

    added about section, video background and some features, fixed bugs

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
-top = document.getElementById("top");
-form = document.getElementById("forms");
-button_order = document.getElementsByClassName("order");
-button_hide = document.getElementById("hide");
+var wrapper = document.querySelector(".wrapper");
+var text = document.querySelector(".about_text");
 
-for (var i = 0; i<button_order.length; i++) {
-  button_order[i].onclick = function scrollTo() {
-      form.style.display = "grid";
-      button_hide.style.display = "grid";
-      window.scrollTo(0, (parseInt(form.scrollHeight)/1.6));
-  }
-}
+var textCont = text.textContent;
+text.style.display = "none";
 
-button_hide.onclick = function hideForm() {
-  form.style.display = "none";
-  button_hide.style.display = "none";
-  window.scrollTo(0, (parseInt(top.scrollHeight)/1.6));
+for (var i = 0; i < textCont.length; i++) {
+  (function(i) {
+    setTimeout(function() {
+      // Created textNode to append
+      var texts = document.createTextNode(textCont[i])
+      var span = document.createElement('span');
+      span.appendChild(texts);
+
+      //Переливание цветов
+      //span.classList.add("wave");
+      wrapper.appendChild(span);
+
+    }, 65 * i);
+  }(i));
 }
