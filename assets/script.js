ece82e190174242c37d87337c039382955c58bc2
Anton Boyko <antoneasymade@gmail.com>
2019-11-08 23:23:38 +0500

    was changed style and added unfinished sidebar

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,6 @@ text.style.display = "none";
 for (var i = 0; i < textCont.length; i++) {
   (function(i) {
     setTimeout(function() {
-      // Created textNode to append
       var texts = document.createTextNode(textCont[i])
       var span = document.createElement('span');
       span.appendChild(texts);
@@ -16,6 +15,22 @@ for (var i = 0; i < textCont.length; i++) {
       //span.classList.add("wave");
       wrapper.appendChild(span);
 
-    }, 65 * i);
+    }, 40 * i);
   }(i));
+}
+
+var sidebar = document.getElementById("sidebar");
+var burger_open = document.getElementById("menu");
+var burger_close = document.getElementById("close_menu");
+
+function sidebar_open() {
+  sidebar.style.display = "grid";
+  burger_open.style.display = "none";
+  burger_close.style.display = "block";
+}
+
+function sidebar_close() {
+  sidebar.style.display = "none";
+  burger_close.style.display = "none";
+  burger_open.style.display = "block";
 }
